50b463128b2bf47e8b92e8c7af12c6e83e78b624
Yusuf Kaan Usta <205040053@stu.istinye.edu.tr>
2025-05-31 00:42:45 +0300

    feat: moves the pickup object with look

--- Assets/Scripts/Pickup System/PlayerPickup.cs
+++ Assets/Scripts/Pickup System/PlayerPickup.cs
@@ -11,9 +11,16 @@ public class PlayerPickup : MonoBehaviour
     [SerializeField] private GameObject pickupCamera;
     [SerializeField] private LayerMask obstacleLayer;
 
+    [SerializeField] private float verticalFollowStrength = 0.5f; // Yukarý aþaðý hareket gücü
+    private float baseYOffset;
+
     private GameObject heldObject;
     private Collider heldCollider;
 
+    void Start()
+    {
+        baseYOffset = pickupParent.localPosition.y;
+    }
 
     void Update()
     {
@@ -25,6 +32,15 @@ public class PlayerPickup : MonoBehaviour
                 Drop();
         }
 
+        // DÜZELTÝLEN KISIM BURASI
+        float camX = Camera.main.transform.eulerAngles.x;
+        float verticalOffset = camX > 180 ? camX - 360 : camX; // Artýk -90 ile +90 arasý
+        verticalOffset = Mathf.Clamp(verticalOffset, -60, 60); // Açý sýnýrý
+        float offsetY = baseYOffset + (verticalOffset / 60f) * verticalFollowStrength;
+        // SON
+
+        Vector3 adjustedPos = pickupParent.parent.TransformPoint(new Vector3(0, offsetY, pickupParent.localPosition.z));
+
         if (heldObject != null)
         {
             Vector3 targetPos = pickupParent.position;
@@ -32,13 +48,12 @@ public class PlayerPickup : MonoBehaviour
 
             //if (!IsObstructed(targetPos, heldCollider.bounds.extents))
             //{
-                heldObject.transform.position = targetPos;
-                heldObject.transform.rotation = targetRot;
+            heldObject.transform.position = adjustedPos;
+            heldObject.transform.rotation = targetRot;
             //}
-            
-
         }
     }
+
     void TryPickup()
     {
         Ray ray = new Ray(transform.position, transform.forward);
@@ -61,6 +76,7 @@ public class PlayerPickup : MonoBehaviour
         heldObject = null;
         heldCollider = null;
     }
+
     bool IsObstructed(Vector3 targetPos, Vector3 extents)
     {
         // Burada objenin boyutlarýna göre çakýþma kontrolü yapýyoruz
